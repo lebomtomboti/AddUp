8a7dca2433405429d029def35ffc935f8814b07d
lebomtomboti <lebomtomboti17@gmail.com>
2024-06-04 15:55:07 +0200

    added text

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //addUp//
+function addArrayValues() {
+    let arrayInput = document.getElementById("arrayInput").value;
+    let array = arrayInput.split(',').map(function(item) {
+      return parseInt(item.trim());
+    });
 let arr = [5,4,3,2,1]
 let addition = 0
 for (let i = 0; i < arr.length; i++) {
     addition += arr[i];
+}
 }
 
 console.log(addition)
